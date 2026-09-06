30810c9467bd412554005149cc775c6e9b2d9a6f
Sharqawy Wael <sherqo@sherqo.me>
2026-09-06 23:01:26 +0300

    add part 3 tests

--- raft_test.go
+++ raft_test.go
@@ -1,9 +1,10 @@
-// Adapted from Eli Bendersky's Raft implementation:
-// https://github.com/eliben/raft
-
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package raft
 
 import (
+	"bytes"
+	"encoding/gob"
 	"testing"
 	"time"
 
@@ -177,13 +178,29 @@ func TestCommitOneCommand(t *testing.T) {
 	origLeaderId, _ := h.CheckSingleLeader()
 
 	tlog("submitting 42 to %d", origLeaderId)
-	isLeader := h.SubmitToServer(origLeaderId, 42)
+	isLeader := h.SubmitToServer(origLeaderId, 42) >= 0
 	if !isLeader {
 		t.Errorf("want id=%d leader, but it's not", origLeaderId)
 	}
 
-	sleepMs(150)
+	sleepMs(250)
 	h.CheckCommittedN(42, 3)
+}
+
+func TestCommitAfterCallDrops(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	lid, _ := h.CheckSingleLeader()
+	h.PeerDropCallsAfterN(lid, 2)
+	h.SubmitToServer(lid, 99)
+	sleepMs(30)
+	h.PeerDontDropCalls(lid)
+
+	sleepMs(60)
+	h.CheckCommittedN(99, 3)
 }
 
 func TestSubmitNonLeaderFails(t *testing.T) {
@@ -193,7 +210,7 @@ func TestSubmitNonLeaderFails(t *testing.T) {
 	origLeaderId, _ := h.CheckSingleLeader()
 	sid := (origLeaderId + 1) % 3
 	tlog("submitting 42 to %d", sid)
-	isLeader := h.SubmitToServer(sid, 42)
+	isLeader := h.SubmitToServer(sid, 42) >= 0
 	if isLeader {
 		t.Errorf("want id=%d !leader, but it is", sid)
 	}
@@ -211,14 +228,14 @@ func TestCommitMultipleCommands(t *testing.T) {
 	values := []int{42, 55, 81}
 	for _, v := range values {
 		tlog("submitting %d to %d", v, origLeaderId)
-		isLeader := h.SubmitToServer(origLeaderId, v)
+		isLeader := h.SubmitToServer(origLeaderId, v) >= 0
 		if !isLeader {
 			t.Errorf("want id=%d leader, but it's not", origLeaderId)
 		}
 		sleepMs(100)
 	}
 
-	sleepMs(150)
+	sleepMs(250)
 	nc, i1 := h.CheckCommitted(42)
 	_, i2 := h.CheckCommitted(55)
 	if nc != 3 {
@@ -259,7 +276,7 @@ func TestCommitWithDisconnectionAndRecover(t *testing.T) {
 
 	// Now reconnect dPeerId and wait a bit; it should find the new command too.
 	h.ReconnectPeer(dPeerId)
-	sleepMs(200)
+	sleepMs(250)
 	h.CheckSingleLeader()
 
 	sleepMs(150)
@@ -318,6 +335,28 @@ func TestNoCommitWithNoQuorum(t *testing.T) {
 	}
 }
 
+func TestDisconnectLeaderBriefly(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	// Submit a couple of values to a fully connected cluster.
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+	h.SubmitToServer(origLeaderId, 6)
+	sleepMs(250)
+	h.CheckCommittedN(6, 3)
+
+	// Disconnect leader for a short time (less than election timeout in peers).
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(90)
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(200)
+
+	h.SubmitToServer(origLeaderId, 7)
+	sleepMs(250)
+	h.CheckCommittedN(7, 3)
+}
+
 func TestCommitsWithLeaderDisconnects(t *testing.T) {
 	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
 
@@ -329,7 +368,7 @@ func TestCommitsWithLeaderDisconnects(t *testing.T) {
 	h.SubmitToServer(origLeaderId, 5)
 	h.SubmitToServer(origLeaderId, 6)
 
-	sleepMs(150)
+	sleepMs(250)
 	h.CheckCommittedN(6, 5)
 
 	// Leader disconnected...
@@ -339,14 +378,14 @@ func TestCommitsWithLeaderDisconnects(t *testing.T) {
 	// Submit 7 to original leader, even though it's disconnected.
 	h.SubmitToServer(origLeaderId, 7)
 
-	sleepMs(150)
+	sleepMs(250)
 	h.CheckNotCommitted(7)
 
 	newLeaderId, _ := h.CheckSingleLeader()
 
 	// Submit 8 to new leader.
 	h.SubmitToServer(newLeaderId, 8)
-	sleepMs(150)
+	sleepMs(250)
 	h.CheckCommittedN(8, 4)
 
 	// Reconnect old leader and let it settle. The old leader shouldn't be the one
@@ -361,10 +400,514 @@ func TestCommitsWithLeaderDisconnects(t *testing.T) {
 
 	// Submit 9 and check it's fully committed.
 	h.SubmitToServer(newLeaderId, 9)
-	sleepMs(150)
+	sleepMs(250)
 	h.CheckCommittedN(9, 5)
 	h.CheckCommittedN(8, 5)
 
 	// But 7 is not committed...
 	h.CheckNotCommitted(7)
+}
+
+func TestCrashFollower(t *testing.T) {
+	// Basic test to verify that crashing a peer doesn't blow up.
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+
+	sleepMs(350)
+	h.CheckCommittedN(5, 3)
+
+	h.CrashPeer((origLeaderId + 1) % 3)
+	sleepMs(350)
+	h.CheckCommittedN(5, 2)
+}
+
+func TestCrashThenRestartFollower(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+	h.SubmitToServer(origLeaderId, 6)
+	h.SubmitToServer(origLeaderId, 7)
+
+	vals := []int{5, 6, 7}
+
+	sleepMs(350)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+
+	h.CrashPeer((origLeaderId + 1) % 3)
+	sleepMs(350)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 2)
+	}
+
+	// Restart the crashed follower and give it some time to come up-to-date.
+	h.RestartPeer((origLeaderId + 1) % 3)
+	sleepMs(650)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+}
+
+func TestCrashThenRestartLeader(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+	h.SubmitToServer(origLeaderId, 6)
+	h.SubmitToServer(origLeaderId, 7)
+
+	vals := []int{5, 6, 7}
+
+	sleepMs(350)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+
+	h.CrashPeer(origLeaderId)
+	sleepMs(350)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 2)
+	}
+
+	h.RestartPeer(origLeaderId)
+	sleepMs(550)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+}
+
+func TestCrashThenRestartAll(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+	h.SubmitToServer(origLeaderId, 6)
+	h.SubmitToServer(origLeaderId, 7)
+
+	vals := []int{5, 6, 7}
+
+	sleepMs(350)
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+
+	for i := 0; i < 3; i++ {
+		h.CrashPeer((origLeaderId + i) % 3)
+	}
+
+	sleepMs(350)
+
+	for i := 0; i < 3; i++ {
+		h.RestartPeer((origLeaderId + i) % 3)
+	}
+
+	sleepMs(150)
+	newLeaderId, _ := h.CheckSingleLeader()
+
+	h.SubmitToServer(newLeaderId, 8)
+	sleepMs(250)
+
+	vals = []int{5, 6, 7, 8}
+	for _, v := range vals {
+		h.CheckCommittedN(v, 3)
+	}
+}
+
+func TestReplaceMultipleLogEntries(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	// Submit a couple of values to a fully connected cluster.
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.SubmitToServer(origLeaderId, 5)
+	h.SubmitToServer(origLeaderId, 6)
+
+	sleepMs(250)
+	h.CheckCommittedN(6, 3)
+
+	// Leader disconnected...
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(10)
+
+	// Submit a few entries to the original leader; it's disconnected, so they
+	// won't be replicated.
+	h.SubmitToServer(origLeaderId, 21)
+	sleepMs(5)
+	h.SubmitToServer(origLeaderId, 22)
+	sleepMs(5)
+	h.SubmitToServer(origLeaderId, 23)
+	sleepMs(5)
+	h.SubmitToServer(origLeaderId, 24)
+	sleepMs(5)
+
+	newLeaderId, _ := h.CheckSingleLeader()
+
+	// Submit entries to new leader -- these will be replicated.
+	h.SubmitToServer(newLeaderId, 8)
+	sleepMs(5)
+	h.SubmitToServer(newLeaderId, 9)
+	sleepMs(5)
+	h.SubmitToServer(newLeaderId, 10)
+	sleepMs(250)
+	h.CheckNotCommitted(21)
+	h.CheckCommittedN(10, 2)
+
+	// Crash/restart new leader to reset its nextIndex, to ensure that the new
+	// leader of the cluster (could be the third server after elections) tries
+	// to replace the original's servers unreplicated entries from the very end.
+	h.CrashPeer(newLeaderId)
+	sleepMs(60)
+	h.RestartPeer(newLeaderId)
+
+	sleepMs(100)
+	finalLeaderId, _ := h.CheckSingleLeader()
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(400)
+
+	// Submit another entry; this is because leaders won't commit entries from
+	// previous terms (paper 5.4.2) so the 8,9,10 may not be committed everywhere
+	// after the restart before a new command comes it.
+	h.SubmitToServer(finalLeaderId, 11)
+	sleepMs(250)
+
+	// At this point, 11 and 10 should be replicated everywhere; 21 won't be.
+	h.CheckNotCommitted(21)
+	h.CheckCommittedN(11, 3)
+	h.CheckCommittedN(10, 3)
+}
+
+func TestCrashAfterSubmit(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	// Wait for a leader to emerge, and submit a command - then immediately
+	// crash; the leader should have no time to send an updated LeaderCommit
+	// to followers. It doesn't have time to get back AE responses either, so
+	// the leader itself won't send it on the commit channel.
+	origLeaderId, _ := h.CheckSingleLeader()
+
+	h.SubmitToServer(origLeaderId, 5)
+	sleepMs(1)
+	h.CrashPeer(origLeaderId)
+
+	// Make sure 5 is not committed when a new leader is elected. Leaders won't
+	// commit commands from previous terms.
+	sleepMs(10)
+	h.CheckSingleLeader()
+	sleepMs(300)
+	h.CheckNotCommitted(5)
+
+	// The old leader restarts. After a while, 5 is still not committed.
+	h.RestartPeer(origLeaderId)
+	sleepMs(150)
+	newLeaderId, _ := h.CheckSingleLeader()
+	h.CheckNotCommitted(5)
+
+	// When we submit a new command, it will be submitted, and so will 5, because
+	// it appears in everyone's logs.
+	h.SubmitToServer(newLeaderId, 6)
+	sleepMs(100)
+	h.CheckCommittedN(5, 3)
+	h.CheckCommittedN(6, 3)
+}
+
+func TestDisconnectAfterSubmit(t *testing.T) {
+	// Similar to TestCrashAfterSubmit, but the leader is disconnected - rather
+	// than crashed - shortly after submitting the first command.
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+
+	h.SubmitToServer(origLeaderId, 5)
+	sleepMs(1)
+	h.DisconnectPeer(origLeaderId)
+
+	// Make sure 5 is not committed when a new leader is elected. Leaders won't
+	// commit commands from previous terms.
+	sleepMs(10)
+	h.CheckSingleLeader()
+	sleepMs(300)
+	h.CheckNotCommitted(5)
+
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(150)
+	newLeaderId, _ := h.CheckSingleLeader()
+	h.CheckNotCommitted(5)
+
+	// When we submit a new command, it will be submitted, and so will 5, because
+	// it appears in everyone's logs.
+	h.SubmitToServer(newLeaderId, 6)
+	sleepMs(100)
+	h.CheckCommittedN(5, 3)
+	h.CheckCommittedN(6, 3)
+}
+
+// getPersistedTerm reads currentTerm from the given storage (same encoding as
+// persistToStorage).
+func getPersistedTerm(storage *MapStorage) int {
+	data, found := storage.Get("currentTerm")
+	if !found {
+		return 0
+	}
+	var term int
+	d := gob.NewDecoder(bytes.NewBuffer(data))
+	if err := d.Decode(&term); err != nil {
+		return 0
+	}
+	return term
+}
+
+func TestBug_StartElectionMissingPersist(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	// Wait for a leader to be elected and pick a non-leader to disconnect.
+	leaderId, _ := h.CheckSingleLeader()
+	victim := (leaderId + 1) % 3
+
+	h.DisconnectPeer(victim)
+
+	// Give it enough time to run a few elections (each bumps currentTerm).
+	time.Sleep(1200 * time.Millisecond)
+
+	// Read the victim's in-memory term and persisted term.
+	cm := h.cluster[victim].cm
+	cm.mu.Lock()
+	inMemoryTerm := cm.currentTerm
+	cm.mu.Unlock()
+
+	persistedTerm := getPersistedTerm(h.storage[victim])
+
+	t.Logf("server %d: in-memory term = %d, persisted term = %d", victim, inMemoryTerm, persistedTerm)
+
+	if persistedTerm < inMemoryTerm {
+		t.Errorf("persisted term (%d) is behind in-memory term (%d); "+
+			"startElection is not persisting state", persistedTerm, inMemoryTerm)
+	}
+}
+
+func TestBug_BecomeFollowerMissingPersist(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, origTerm := h.CheckSingleLeader()
+
+	// Isolate the leader so the other two servers can elect a new leader in a
+	// higher term.
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(350)
+
+	newLeaderId, newTerm := h.CheckSingleLeader()
+	if newTerm <= origTerm {
+		t.Fatalf("got newTerm=%d, origTerm=%d; want newTerm > origTerm", newTerm, origTerm)
+	}
+
+	// Keep the new leader from sending messages to the old one after it
+	// reconnects. This way, when the old leader comes back, it can notice the
+	// newer term and step down, but it won't later receive a fresh heartbeat
+	// that could update its stored term before we crash it.
+	h.PeerDropCallsAfterN(newLeaderId, 0)
+	defer h.PeerDontDropCalls(newLeaderId)
+
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(120)
+
+	_, steppedDownTerm, isLeader := h.cluster[origLeaderId].cm.Report()
+	if isLeader {
+		t.Fatalf("server %d still thinks it's leader after reconnect", origLeaderId)
+	}
+	if steppedDownTerm != newTerm {
+		t.Fatalf("server %d has term %d after step-down; want %d", origLeaderId, steppedDownTerm, newTerm)
+	}
+
+	// Crash immediately after the old leader observes the newer term and stops
+	// leading. On restart, it should still remember that higher term. If not,
+	// it means the term change was only kept in memory and was lost on crash.
+	h.CrashPeer(origLeaderId)
+	h.RestartPeer(origLeaderId)
+
+	_, restartedTerm, _ := h.cluster[origLeaderId].cm.Report()
+	if restartedTerm != newTerm {
+		t.Fatalf("server %d restarted with term %d; want persisted higher term %d", origLeaderId, restartedTerm, newTerm)
+	}
+}
+
+// A follower that receives becomeFollower with the same term it is already in
+// must keep its votedFor intact. Resetting votedFor on a same-term transition
+// would allow the node to vote twice in the same term, violating Raft's
+// vote-once-per-term safety property.
+func TestBecomeFollowerSameTermPreservesVotedFor(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	h.CheckSingleLeader()
+
+	for i := 0; i < 3; i++ {
+		cm := h.cluster[i].cm
+		cm.mu.Lock()
+		if cm.state == Follower && cm.votedFor >= 0 {
+			savedVotedFor := cm.votedFor
+			savedTerm := cm.currentTerm
+
+			cm.becomeFollower(savedTerm)
+
+			if cm.votedFor != savedVotedFor {
+				t.Errorf("becomeFollower(%d) reset votedFor from %d to %d on same-term transition",
+					savedTerm, savedVotedFor, cm.votedFor)
+			}
+			cm.mu.Unlock()
+			return
+		}
+		cm.mu.Unlock()
+	}
+	t.Fatal("no follower with votedFor >= 0 found")
+}
+
+// A follower that transitions to a higher term must reset votedFor to -1,
+// so it is free to vote in the new term. Without this reset, the node would
+// refuse all vote requests in the new term, potentially preventing leader
+// election.
+func TestBecomeFollowerHigherTermResetsVotedFor(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	h.CheckSingleLeader()
+
+	for i := 0; i < 3; i++ {
+		cm := h.cluster[i].cm
+		cm.mu.Lock()
+		if cm.state == Follower && cm.votedFor >= 0 {
+			savedTerm := cm.currentTerm
+
+			cm.becomeFollower(savedTerm + 1)
+
+			if cm.votedFor != -1 {
+				t.Errorf("becomeFollower(%d) did not reset votedFor (got %d, want -1)",
+					savedTerm+1, cm.votedFor)
+			}
+			cm.mu.Unlock()
+			return
+		}
+		cm.mu.Unlock()
+	}
+	t.Fatal("no follower with votedFor >= 0 found")
+}
+
+// After multiple leader changes, reconnected nodes with stale terms must not
+// disrupt the cluster. This tests that a formerly-partitioned leader and a
+// second leader can rejoin without causing a split-brain or election loop.
+func TestStaleVoteReplyIgnored(t *testing.T) {
+	h := NewHarness(t, 5)
+	defer h.Shutdown()
+
+	origLeaderId, origTerm := h.CheckSingleLeader()
+
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(450)
+
+	newLeaderId, newTerm := h.CheckSingleLeader()
+	if newTerm <= origTerm {
+		t.Fatalf("expected newTerm > origTerm, got %d <= %d", newTerm, origTerm)
+	}
+
+	h.DisconnectPeer(newLeaderId)
+	sleepMs(450)
+
+	h.ReconnectPeer(origLeaderId)
+	h.ReconnectPeer(newLeaderId)
+	sleepMs(450)
+
+	h.CheckSingleLeader()
+}
+
+// A follower that already voted for a leader in this term must reject a
+// RequestVote from a different candidate in the same term. Granting a second
+// vote would allow two leaders to be elected in the same term.
+func TestSameTermDoubleVotePrevented(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	leaderId, leaderTerm := h.CheckSingleLeader()
+
+	followerId := -1
+	for i := 0; i < 3; i++ {
+		if i == leaderId {
+			continue
+		}
+		cm := h.cluster[i].cm
+		cm.mu.Lock()
+		if cm.votedFor == leaderId && cm.currentTerm == leaderTerm {
+			followerId = i
+		}
+		cm.mu.Unlock()
+		if followerId >= 0 {
+			break
+		}
+	}
+	if followerId < 0 {
+		t.Fatal("could not find a follower that voted for the leader")
+	}
+
+	otherCandidate := -1
+	for i := 0; i < 3; i++ {
+		if i != leaderId && i != followerId {
+			otherCandidate = i
+			break
+		}
+	}
+
+	cm := h.cluster[followerId].cm
+	args := RequestVoteArgs{
+		Term:         leaderTerm,
+		CandidateId:  otherCandidate,
+		LastLogIndex: -1,
+		LastLogTerm:  -1,
+	}
+	var reply RequestVoteReply
+	if err := cm.RequestVote(args, &reply); err != nil {
+		t.Fatal(err)
+	}
+
+	if reply.VoteGranted {
+		t.Errorf("follower %d granted vote to %d in term %d, but already voted for %d",
+			followerId, otherCandidate, leaderTerm, leaderId)
+	}
+}
+
+// Stress test: repeatedly partition the current leader and verify that the
+// cluster always converges to exactly one leader after each disruption.
+func TestElectionSafetyStress(t *testing.T) {
+	h := NewHarness(t, 5)
+	defer h.Shutdown()
+
+	for cycle := 0; cycle < 8; cycle++ {
+		leaderId, _ := h.CheckSingleLeader()
+		h.DisconnectPeer(leaderId)
+		sleepMs(350)
+
+		h.CheckSingleLeader()
+
+		h.ReconnectPeer(leaderId)
+		sleepMs(150)
+	}
+
+	time.Sleep(300 * time.Millisecond)
+	h.CheckSingleLeader()
 }
